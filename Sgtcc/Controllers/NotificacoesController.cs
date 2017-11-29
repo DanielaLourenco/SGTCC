710d5657fb01186e59c6705d6295ff5323797389
Daniela Lourenco <lourencodaniela.s@gmail.com>
2017-11-28 23:58:57 -0200

    Noticacao de Defesa Aproximando

--- Sgtcc/Controllers/NotificacoesController.cs
+++ Sgtcc/Controllers/NotificacoesController.cs
@@ -12,19 +12,21 @@ namespace Sgtcc.Controllers
 {
     public class NotificacoesController : Controller
     {
-        private Model1Container db = new Model1Container(); 
+        private Model1Container db = new Model1Container();
         // GET: Notificacoes
         public ActionResult Index()
         {
 
-            //Determina se esta em periodo de envio de alertas
+            //Determina se esta em periodo de envio de alertas            
             string dataAgendamento = PeriodoAlertaAgendamento(db.CronogramaAgendamentoes.ToList());
-            if (dataAgendamento != null)
-                EnviarEmailAgendamento(db.Tccs2.ToList(), dataAgendamento);
             string dataSubmissao = PeriodoAlertaSubmissao(db.CronogramaArquivoes.ToList());
+
+            if (dataAgendamento != null)
+                EnviarEmailAgendamento(db.Tccs2.ToList(), dataAgendamento);           
             if (dataSubmissao != null)
                 EnviarEmailSubmissao(db.Alunos.ToList(), db.Arquivoes.ToList(), dataSubmissao);
-
+            //Verica a proximidade da defesa e envia alerta
+            EnviarEmailDefesa(db.Tccs2.ToList());
             return View();
         }
 
@@ -101,6 +103,45 @@ namespace Sgtcc.Controllers
             }
         }
 
+        private static void EnviarEmailDefesa(List<Tcc2> tccList)
+        {
+            try
+            {
+                NetworkCredential login = new NetworkCredential("sgtcccefet@gmail.com", "123sgtcc");
+                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+                //Obter lista de tccs que ja tem arquivo
+                List<Tcc> tccAgendadoList = new List<Tcc>();
+                DateTime dataAtual = DateTime.Now;
+
+                foreach (Tcc2 tcc in tccList)
+                {
+                    if (tcc.data != "")
+                        tccAgendadoList.Add(tcc);
+                }
+
+                foreach (Tcc2 tcc in tccAgendadoList)
+                {
+                    TimeSpan diff1 = dataAtual.Subtract((Convert.ToDateTime(tcc.data)));
+                    if (diff1.TotalDays <= 5)
+                        email.To.Add(new MailAddress(tcc.Aluno.email));
+                }
+
+                email.From = new MailAddress("sgtcccefet@gmail.com");
+                email.Subject = "Alerta - SGTCC";
+                email.Body = "Sua defesa de TCC está se aproximando. Se prepare e tenha faça ótima apresentação  ";
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = login;
+                client.Send(email);
+            }
+            catch
+            {
+
+            }
+        }
+
         private static string PeriodoAlertaAgendamento(List<CronogramaAgendamento> agendamentoList)
         {
             DateTime dataAtual = DateTime.Now;
@@ -110,7 +151,7 @@ namespace Sgtcc.Controllers
                 DateTime prazoInicio = Convert.ToDateTime(agenda.prazoInicial);
                 DateTime prazoFim = Convert.ToDateTime(agenda.prazoFinal);
                 if (DateTime.Compare(prazoInicio, dataAtual) < 0 && DateTime.Compare(dataAtual, prazoFim) > 0)
-                {                    
+                {
                     return agenda.prazoFinal.ToString();
                 }
             }
@@ -127,7 +168,7 @@ namespace Sgtcc.Controllers
                 DateTime prazoInicio = Convert.ToDateTime(agendaSubmissao.prazoInicial);
                 DateTime prazoFim = Convert.ToDateTime(agendaSubmissao.prazoFinal);
                 if (DateTime.Compare(prazoInicio, dataAtual) < 0 && DateTime.Compare(dataAtual, prazoFim) > 0)
-                {                     
+                {
                     return agendaSubmissao.prazoFinal.ToString();
                 }
             }
