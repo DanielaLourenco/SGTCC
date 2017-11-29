a7c517f1477f0f18583339321ebaac85b46865e9
Daniela Lourenco <lourencodaniela.s@gmail.com>
2017-11-28 22:05:20 -0200

    Notificaoes

--- Sgtcc/Controllers/HomeController.cs
+++ Sgtcc/Controllers/HomeController.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using Sgtcc.Models;
 
 namespace Sgtcc.Controllers
 {
     public class HomeController : Controller
     {
+        private Model1Container db = new Model1Container();
+
         public ActionResult Index()
         {
+            //Determina se esta em periodo de envio de alertas
+            if(PeriodoAlertaAgendamento(db.CronogramaAgendamentoes.ToList()))
+                EnviarEmailAgendamento(db.Tccs2.ToList());
+            if (PeriodoAlertaSubmissao(db.CronogramaArquivoes.ToList()))
+                EnviarEmailSubmissao(db.Alunos.ToList(), db.Arquivoes.ToList());
+            
             return View();
         }
 
@@ -25,6 +36,117 @@ namespace Sgtcc.Controllers
             ViewBag.Message = "SGTCC - Contato.";
 
             return View();
+        }
+        private static void EnviarEmailSubmissao(List<Aluno> destinarioList, List<Arquivo> arquivoList)
+        {
+            try
+            {
+                NetworkCredential login = new NetworkCredential("sgtcccefet@gmail.com", "123sgtcc");
+                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+                //Obter lista de tccs que ja tem arquivo
+                List<String> tccsComArquivo = new List<string>();
+                foreach (Arquivo arquivo in arquivoList)
+                {
+                    tccsComArquivo.Add(arquivo.Tcc.Id.ToString());
+                }
+
+                foreach (Aluno aluno in destinarioList)
+                {
+                    if (!tccsComArquivo.Contains(aluno.Id.ToString()))
+                        email.To.Add(new MailAddress(aluno.email));
+                }
+              
+                email.From = new MailAddress("sgtcccefet@gmail.com");
+                email.Subject = "Alerta - SGTCC";
+
+                email.Body = "Você ainda não submeteu o seu arquivo de TCC. Fique atento, o prazo é até: ";
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = login;
+                client.Send(email);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private static void EnviarEmailAgendamento(List<Tcc2> tccList)
+        {
+            try
+            {
+                NetworkCredential login = new NetworkCredential("sgtcccefet@gmail.com", "123sgtcc");
+                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+                //Obter lista de tccs que ja tem arquivo
+                List<Aluno> alunoSemAgendamentoList = new List<Aluno>();
+                foreach (Tcc2 tcc in tccList)
+                {
+                    if (tcc.data == "")
+                        alunoSemAgendamentoList.Add(tcc.Aluno);
+                }
+
+                foreach (Aluno aluno in alunoSemAgendamentoList)
+                {
+
+                    email.To.Add(new MailAddress(aluno.email));
+                }
+                              
+                email.From = new MailAddress("sgtcccefet@gmail.com");
+                email.Subject = "Alerta - SGTCC";
+
+                email.Body = "Você ainda não agendou sua defesa de TCC. Fique atento, o prazo é até: ";
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = login;
+                client.Send(email);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private static bool PeriodoAlertaAgendamento(List<CronogramaAgendamento> agendamentoList)
+        {
+            DateTime dataAtual = DateTime.Now;
+
+            foreach (CronogramaAgendamento agenda in agendamentoList)
+            {
+                DateTime prazoInicio = Convert.ToDateTime(agenda.prazoInicial.ToString());
+                DateTime prazoFim = Convert.ToDateTime(agenda.prazoFinal.ToString());
+
+                if (DateTime.Compare(prazoInicio, dataAtual) < 0 && DateTime.Compare(dataAtual, prazoFim) < 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static bool PeriodoAlertaSubmissao(List<CronogramaArquivo> submissaoList)
+        {
+            DateTime dataAtual = DateTime.Now;
+
+            foreach (CronogramaArquivo agendaSubmissao in submissaoList)
+            {
+                DateTime prazoInicio = Convert.ToDateTime(agendaSubmissao.prazoInicial.ToString());
+                DateTime prazoFim = Convert.ToDateTime(agendaSubmissao.prazoFinal.ToString());
+
+                if (DateTime.Compare(prazoInicio, dataAtual) < 0 && DateTime.Compare(dataAtual, prazoFim) < 0)
+                    return true;
+            }
+            
+            return false;
+        }
+
+        public bool VerificaEnvio() {
+            string dateString = "5/1/2008 8:30:52 AM";
+            DateTime date1 = DateTime.Parse(dateString,
+                                      System.Globalization.CultureInfo.InvariantCulture);
+            return true;
         }
     }
 }
--- Sgtcc/Models/Tcc.cs
+++ Sgtcc/Models/Tcc.cs
@@ -6,6 +6,8 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sgtcc.Models
 {
@@ -21,9 +23,13 @@ namespace Sgtcc.Models
         }
     
         public int Id { get; set; }
+        [Display(Name = "Título")]
         public string titulo { get; set; }
+        [Display(Name = "Semestre")]
         public string semestre { get; set; }
+        [Display(Name = "Ano")]
         public string ano { get; set; }
+        [Display(Name = "Status")]
         public string status { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
