020617f693c2cce2029255326e42afd736eaed2f
Daniela Lourenco <lourencodaniela.s@gmail.com>
2017-11-28 23:16:25 -0200

    Notificacoes - "Finalizado"

---
+++ Sgtcc/Controllers/NotificacoesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using Sgtcc.Models;
+
+
+namespace Sgtcc.Controllers
+{
+    public class NotificacoesController : Controller
+    {
+        private Model1Container db = new Model1Container(); 
+        // GET: Notificacoes
+        public ActionResult Index()
+        {
+
+            //Determina se esta em periodo de envio de alertas
+            string dataAgendamento = PeriodoAlertaAgendamento(db.CronogramaAgendamentoes.ToList());
+            if (dataAgendamento != null)
+                EnviarEmailAgendamento(db.Tccs2.ToList(), dataAgendamento);
+            string dataSubmissao = PeriodoAlertaSubmissao(db.CronogramaArquivoes.ToList());
+            if (dataSubmissao != null)
+                EnviarEmailSubmissao(db.Alunos.ToList(), db.Arquivoes.ToList(), dataSubmissao);
+
+            return View();
+        }
+
+        private static void EnviarEmailSubmissao(List<Aluno> destinarioList, List<Arquivo> arquivoList, string dataSubmissao)
+        {
+            try
+            {
+                NetworkCredential login = new NetworkCredential("sgtcccefet@gmail.com", "123sgtcc");
+                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+                //Obter lista de tccs que ja tem arquivo
+                List<String> tccsComArquivo = new List<string>();
+                foreach (Arquivo arquivo in arquivoList)
+                {
+                    tccsComArquivo.Add(arquivo.Tcc.Id.ToString());
+                }
+
+                foreach (Aluno aluno in destinarioList)
+                {
+                    if (!tccsComArquivo.Contains(aluno.Id.ToString()))
+                        email.To.Add(new MailAddress(aluno.email));
+                }
+
+                email.From = new MailAddress("sgtcccefet@gmail.com");
+                email.Subject = "Alerta - SGTCC";
+
+                email.Body = "Você ainda não submeteu o seu arquivo de TCC. Fique atento, o prazo é até: " + dataSubmissao;
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = login;
+                client.Send(email);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private static void EnviarEmailAgendamento(List<Tcc2> tccList, string dataAgendamento)
+        {
+            try
+            {
+                NetworkCredential login = new NetworkCredential("sgtcccefet@gmail.com", "123sgtcc");
+                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+                //Obter lista de tccs que ja tem arquivo
+                List<Aluno> alunoSemAgendamentoList = new List<Aluno>();
+                foreach (Tcc2 tcc in tccList)
+                {
+                    if (tcc.data == "")
+                        alunoSemAgendamentoList.Add(tcc.Aluno);
+                }
+
+                foreach (Aluno aluno in alunoSemAgendamentoList)
+                {
+
+                    email.To.Add(new MailAddress(aluno.email));
+                }
+
+                email.From = new MailAddress("sgtcccefet@gmail.com");
+                email.Subject = "Alerta - SGTCC";
+
+                email.Body = "Você ainda não agendou sua defesa de TCC. Fique atento, o prazo é até: " + dataAgendamento;
+
+                SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = login;
+                client.Send(email);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private static string PeriodoAlertaAgendamento(List<CronogramaAgendamento> agendamentoList)
+        {
+            DateTime dataAtual = DateTime.Now;
+
+            foreach (CronogramaAgendamento agenda in agendamentoList)
+            {
+                DateTime prazoInicio = Convert.ToDateTime(agenda.prazoInicial);
+                DateTime prazoFim = Convert.ToDateTime(agenda.prazoFinal);
+                if (DateTime.Compare(prazoInicio, dataAtual) < 0 && DateTime.Compare(dataAtual, prazoFim) > 0)
+                {                    
+                    return agenda.prazoFinal.ToString();
+                }
+            }
+
+            return null;
+        }
+
+        private static string PeriodoAlertaSubmissao(List<CronogramaArquivo> submissaoList)
+        {
+            DateTime dataAtual = DateTime.Now;
+
+            foreach (CronogramaArquivo agendaSubmissao in submissaoList)
+            {
+                DateTime prazoInicio = Convert.ToDateTime(agendaSubmissao.prazoInicial);
+                DateTime prazoFim = Convert.ToDateTime(agendaSubmissao.prazoFinal);
+                if (DateTime.Compare(prazoInicio, dataAtual) < 0 && DateTime.Compare(dataAtual, prazoFim) > 0)
+                {                     
+                    return agendaSubmissao.prazoFinal.ToString();
+                }
+            }
+
+            return null;
+        }
+    }
+}
